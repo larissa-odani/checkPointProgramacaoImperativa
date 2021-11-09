025dae8688c7f832b8a51d8c8360262de1fc3a5e
larissa-odani <larissa.odani@gmail.com>
2021-11-09 20:38:00 -0300

    envio checkpoint01 larissa

---
+++ checkPointEntrega.js
@@ -0,0 +1,79 @@
+const pipoca = (tempo) => {
+    if (tempo == 10){
+        console.log("Prato pronto, bom apetite!!!")
+    }else if (tempo < 10){
+        console.log("Tempo insuficiente ")
+    }else if (tempo > 20 && tempo < 30){
+        console.log("A comida queimou.")
+    }else if (tempo >= 30){
+        console.log("kabumm")
+    }else{
+        console.log("Prato inexistente")
+    }
+}   
+
+//pipoca(30);
+
+const macarrao = (tempo) => {
+    if (tempo == 8){
+        console.log("Prato pronto, bom apetite!!!")
+    }else if (tempo < 8){
+        console.log("Tempo insuficiente ")
+    }else if (tempo > 16 && tempo < 24){
+        console.log("A comida queimou.")
+    }else if (tempo >= 24){
+        console.log("kabumm")
+    }else{
+        console.log("Prato inexistente")
+    }
+}   
+
+//macarrao(8);
+
+const carne = (tempo) => {
+    if (tempo == 15){
+        console.log("Prato pronto, bom apetite!!!")
+    }else if (tempo < 15){
+        console.log("Tempo insuficiente ")
+    }else if (tempo > 30 && tempo < 45){
+        console.log("A comida queimou.")
+    }else if (tempo >= 45){
+        console.log("kabumm")
+    }else{
+        console.log("Prato inexistente")
+    }
+}   
+
+//carne(35);
+
+const feijao = (tempo) => {
+    if (tempo == 12){
+        console.log("Prato pronto, bom apetite!!!")
+    }else if (tempo < 12){
+        console.log("Tempo insuficiente ")
+    }else if (tempo > 24 && tempo < 36){
+        console.log("A comida queimou.")
+    }else if (tempo >= 36){
+        console.log("kabumm")
+    }else{
+        console.log("Prato inexistente")
+    }
+}   
+
+//feijao(11);
+
+const brigadeiro = (tempo) => {
+    if (tempo == 8){
+        console.log("Prato pronto, bom apetite!!!")
+    }else if (tempo < 8){
+        console.log("Tempo insuficiente ")
+    }else if (tempo > 16 && tempo < 24){
+        console.log("A comida queimou.")
+    }else if (tempo >= 24){
+        console.log("kabumm")
+    }else{
+        console.log("Prato inexistente")
+    }
+}   
+
+//brigadeiro(25);
