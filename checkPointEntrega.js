b22b326c334ad9e0b7d2bc16977ece70626d1495
larissa.odani <larissa.odani@gmail.com>
2021-11-09 21:15:05 -0300

    modificacao

--- checkPointEntrega.js
+++ checkPointEntrega.js
@@ -1,11 +1,11 @@
 const pipoca = (tempo) => {
-    if (tempo == 10){
-        console.log("Prato pronto, bom apetite!!!")
-    }else if (tempo < 10){
+    if (tempo < 10){
         console.log("Tempo insuficiente ")
-    }else if (tempo > 20 && tempo < 30){
+    }else if (tempo == 10){
+        console.log("Prato pronto, bom apetite!!!")
+    }else if (tempo > 2*10 && tempo < 3*10){
         console.log("A comida queimou.")
-    }else if (tempo >= 30){
+    }else if (tempo >= 3*10){
         console.log("kabumm")
     }else{
         console.log("Prato inexistente")
@@ -15,13 +15,13 @@ const pipoca = (tempo) => {
 //pipoca(30);
 
 const macarrao = (tempo) => {
-    if (tempo == 8){
-        console.log("Prato pronto, bom apetite!!!")
-    }else if (tempo < 8){
+    if (tempo < 8){
         console.log("Tempo insuficiente ")
-    }else if (tempo > 16 && tempo < 24){
+    }else if (tempo == 8){
+        console.log("Prato pronto, bom apetite!!!")
+    }else if (tempo > 2*8 && tempo < 3*8){
         console.log("A comida queimou.")
-    }else if (tempo >= 24){
+    }else if (tempo >= 3*8){
         console.log("kabumm")
     }else{
         console.log("Prato inexistente")
@@ -31,13 +31,13 @@ const macarrao = (tempo) => {
 //macarrao(8);
 
 const carne = (tempo) => {
-    if (tempo == 15){
-        console.log("Prato pronto, bom apetite!!!")
-    }else if (tempo < 15){
+    if (tempo < 15){
         console.log("Tempo insuficiente ")
-    }else if (tempo > 30 && tempo < 45){
+    }else if (tempo == 15){
+        console.log("Prato pronto, bom apetite!!!")
+    }else if (tempo > 2*15 && tempo < 3*15){
         console.log("A comida queimou.")
-    }else if (tempo >= 45){
+    }else if (tempo >= 3*15){
         console.log("kabumm")
     }else{
         console.log("Prato inexistente")
@@ -47,13 +47,13 @@ const carne = (tempo) => {
 //carne(35);
 
 const feijao = (tempo) => {
-    if (tempo == 12){
-        console.log("Prato pronto, bom apetite!!!")
-    }else if (tempo < 12){
+    if (tempo < 12){
         console.log("Tempo insuficiente ")
-    }else if (tempo > 24 && tempo < 36){
+    }else if (tempo == 12){
+        console.log("Prato pronto, bom apetite!!!")
+    }else if (tempo > 2*12 && tempo < 3*12){
         console.log("A comida queimou.")
-    }else if (tempo >= 36){
+    }else if (tempo >= 3*12){
         console.log("kabumm")
     }else{
         console.log("Prato inexistente")
@@ -63,17 +63,17 @@ const feijao = (tempo) => {
 //feijao(11);
 
 const brigadeiro = (tempo) => {
-    if (tempo == 8){
-        console.log("Prato pronto, bom apetite!!!")
-    }else if (tempo < 8){
+    if (tempo < 8){
         console.log("Tempo insuficiente ")
-    }else if (tempo > 16 && tempo < 24){
+    }else if (tempo == 8){
+        console.log("Prato pronto, bom apetite!!!")
+    }else if (tempo > 2*8 && tempo < 3*8){
         console.log("A comida queimou.")
-    }else if (tempo >= 24){
+    }else if (tempo >= 3*8){
         console.log("kabumm")
     }else{
         console.log("Prato inexistente")
     }
 }   
 
-//brigadeiro(25);
+//brigadeiro(10);
